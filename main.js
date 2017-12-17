938fc6c8b0eb458d7276d7c3b521cb86696f4d10
Chi Nguyen <chi.nguyen.idean@gmail.com>
2017-12-18 00:21:46 +0200

    sida vlon

--- main.js
+++ main.js
@@ -43,49 +43,24 @@ function showMovie(){
     xmlhttp.onreadystatechange = function() {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
             var schedule = xmlhttp.responseXML;
-            var scheduleid = schedule.getElementsByTagName("TheatreID");
+            var movies = schedule.getElementsByTagName("TheatreID");
             var dd =  document.getElementById("dd");
-            var value = dd.options[dd.selectedIndex].value;
+            var selectedTheaterId = dd.options[dd.selectedIndex].value;
             
-
-            for (i=0;i< scheduleid.length; i ++){
-                if (value = scheduleid){
-                    var name =  scheduleid[i].parentNode.children[15].innerHTML;
-                    var duration = scheduleid[i].parentNode.children[18].innerHTML;
-                    var theater = scheduleid[i].parentNode.children[27].innerHTML;
-                    var image  = scheduleid[i].parentNode.children[35].children[4].innerHTML;
-                    scheduleid[i].parentNode.className = "hello";
-                    var a = document.getElementsByClassName("hello");
-
-                    var table = "<table>";
-                    for (i=0; i<a.length;i++ ){
-                        table += '<tr>'+'<td>' + name +'</td>';
-                        table += '<td>' + duration + '</td>';
-                        table += '<td>' + theater + '</td>';
-                        table += '<td>' + image + '</td>' + '</tr>';
-
-                    }
-                    table += "</table>";
-
-
-
-                }
-                document.getElementById("schedule").innerHTML = table;
+            var table = "<table>";
+      
+            for (i=0;i< movies.length; i ++){
+                var name =  movies[i].parentNode.children[15].innerHTML;
+                var duration = movies[i].parentNode.children[18].innerHTML;
+                var theater = movies[i].parentNode.children[27].innerHTML;
+                var image  = movies[i].parentNode.children[35].children[4].innerHTML;
+                table += '<tr>'+'<td>' + name +'</td>';
+                table += '<td>' + duration + '</td>';
+                table += '<td>' + theater + '</td>';
+                table += '<td>' + image + '</td>' + '</tr>';
             }
-
-
-           // if (value == scheduleid){
-              //  for (i=0;){
-              //  var table = '<table id ="tb">';
-                //var movies = schedule.getElementsByTagName("Title");
-            //    }
-                
-          //  }
-            //var theater = schedule.getElementsByTagName("");
-            
-
-           // if (value =  ){}
-
+            table += "</table>";
+            document.getElementById("schedule").innerHTML = table;
         }
     }
 
